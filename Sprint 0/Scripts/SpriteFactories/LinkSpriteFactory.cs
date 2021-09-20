2f3af6da4f0b014f7abeffc2c3cee44b13ea29b2
Nicholas Brinkel <Nickbrinkel@gmail.com>
2021-09-20 13:07:12 -0400

    Created Sprite Factory class for Link

---
+++ Sprint 0/Scripts/SpriteFactories/LinkSpriteFactory.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint_0.Scripts.SpriteFactories
+{
+    public class LinkSpriteFactory
+    {
+        private Texture2D spriteSheet;
+
+		private static LinkSpriteFactory instance = new LinkSpriteFactory();
+
+		public static LinkSpriteFactory Instance
+		{
+			get
+			{
+				return instance;
+			}
+		}
+
+		private LinkSpriteFactory()
+		{
+		}
+
+		public void LoadAllTextures(ContentManager content)
+		{
+			spriteSheet = content.Load<Texture2D>("LinkSpriteSheet");
+		}
+	}
+}
