1e44cf79bc2247074bfd3bda30c86d8f94680f17
Nicholas Brinkel <Nickbrinkel@gmail.com>
2021-10-04 20:40:22 -0400

    Basic arrow and boomerang now spawn from Link's position too

--- Sprint 0/Scripts/Commands/CommandShootBasicArrow.cs
+++ Sprint 0/Scripts/Commands/CommandShootBasicArrow.cs
@@ -5,17 +5,17 @@ namespace Sprint_0.Scripts.Commands
     public class CommandShootBasicArrow : ICommand
     {
         private Game1 game;
+        private Link link;
 
         public CommandShootBasicArrow(Game1 game)
         {
-            // TODO: Add ILink as parameter
             this.game = game;
+            this.link = game.link;
         }
 
         public void Execute()
         {
-            // TODO: Replace position and direction with those of Link
-            game.itemSet.items.Add(ItemFactory.Instance.CreateArrow(game.GetCenterScreen(), FacingDirection.Right, false));
+            game.itemSet.items.Add(ItemFactory.Instance.CreateArrow(link.ItemSpawnPosition, link.FacingDirection, false));
         }
     }
 }
