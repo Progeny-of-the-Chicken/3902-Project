c158b0208ea4bda50b4e5b669f2de854594f5348
lucasbutlerr <butler.1032@osu.edu>
2021-10-12 23:35:32 -0400

    Split items and projectiles, removed treasure term

---
+++ Sprint 0/Scripts/Projectiles/ProjectileEntities.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Sprint_0.Scripts.Projectiles
+{
+    public class ProjectileEntities
+    {
+        public ProjectileEntities()
+        {
+        }
+    }
+}
---
+++ Sprint 0/Scripts/Projectiles/ProjectileFactory.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Sprint_0.Scripts.Projectiles
+{
+    public class ProjectileFactory
+    {
+        public ProjectileFactory()
+        {
+        }
+    }
+}
