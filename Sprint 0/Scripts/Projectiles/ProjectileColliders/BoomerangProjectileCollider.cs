857c8ffa3a4597b5a80f0e05b8826e4b778af5fc
lucasbutlerr <butler.1032@osu.edu>
2021-10-24 14:10:05 -0400

    Added bomb collider and firespell collider.

--- Sprint 0/Scripts/Projectiles/ProjectileColliders/BoomerangProjectileCollider.cs
+++ Sprint 0/Scripts/Projectiles/ProjectileColliders/BoomerangProjectileCollider.cs
@@ -1,5 +1,4 @@
 ﻿using Microsoft.Xna.Framework;
-using Sprint_0.Scripts.Projectiles.ProjectileClasses;
 using Sprint_0.Scripts.Enemy;
 
 namespace Sprint_0.Scripts.Projectiles.ProjectileColliders
@@ -7,7 +6,6 @@ namespace Sprint_0.Scripts.Projectiles.ProjectileColliders
     public class BoomerangProjectileCollider : IProjectileCollider
     {
         private Rectangle _hitbox;
-        private Vector2 positionOffset;
         private FacingDirection direction;
 
         public IProjectile Owner { get; }
@@ -17,13 +15,14 @@ namespace Sprint_0.Scripts.Projectiles.ProjectileColliders
         public BoomerangProjectileCollider(IProjectile owner, FacingDirection direction)
         {
             this.Owner = owner;
+            this.direction = direction;
             // No size variation between basic and magical boomerangs
             _hitbox = SpriteRectangles.basicBoomerangFrames[0];
         }
 
         public void Update(Vector2 location)
         {
-            _hitbox.Location = location.ToPoint() + positionOffset.ToPoint();
+            _hitbox.Location = location.ToPoint();
         }
 
         public void OnPlayerCollision(ILink link)
