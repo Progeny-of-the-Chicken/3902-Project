a9b33e26bd6867b2df41688a3e4bc2716bd506ca
lucasbutlerr <butler.1032@osu.edu>
2021-10-24 16:33:22 -0400

    Starter factory for projectile colliders.

---
+++ Sprint 0/Scripts/Projectiles/ProjectileColliders/ProjectileColliderFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace Sprint_0.Scripts.Projectiles.ProjectileColliders
+{
+    public class ProjectileColliderFactory
+    {
+        private static ProjectileColliderFactory instance = new ProjectileColliderFactory();
+
+        public static ProjectileColliderFactory Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private ProjectileColliderFactory()
+        {
+        }
+    }
+}
