b6690de8f60a560c38727c1d447419e5bc5d3e7f
lucasbutlerr <butler.1032@osu.edu>
2021-10-24 23:33:13 -0400

    Now tracking colliders.

---
+++ Sprint 0/Scripts/Collider/Projectile/BoomerangProjectileCollider.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using Sprint_0.Scripts.Projectiles;
+using Sprint_0.Scripts.Enemy;
+
+namespace Sprint_0.Scripts.Collider.Projectile
+{
+    public class BoomerangProjectileCollider : IProjectileCollider
+    {
+        private Rectangle _hitbox;
+
+        public IProjectile Owner { get; }
+
+        public Rectangle Hitbox { get => _hitbox; }
+
+        public BoomerangProjectileCollider(IProjectile owner)
+        {
+            this.Owner = owner;
+            // No size variation between basic and magical boomerangs
+            _hitbox = SpriteRectangles.basicBoomerangFrames[0];
+            _hitbox.Size *= new Point(ObjectConstants.scale);
+        }
+
+        public void Update(Vector2 location)
+        {
+            _hitbox.Location = location.ToPoint();
+        }
+
+        public void OnPlayerCollision(Link link)
+        {
+            if (!Owner.Friendly)
+            {
+                link.TakeDamage(Owner.Damage);
+                // TODO: knockback link
+            }
+        }
+
+        public void OnEnemyCollision(IEnemy enemy)
+        {
+            if (Owner.Friendly)
+            {
+                enemy.TakeDamage(Owner.Damage);
+                enemy.KnockBack(Overlap.DirectionToMoveObjectOff(_hitbox, enemy.Collider.collisionRectangle));
+            }
+        }
+    }
+}
--- Sprint 0/Scripts/Items/IItem.cs
+++ Sprint 0/Scripts/Items/IItem.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework;
-using Sprint_0.Scripts.Collider.ItemColliders;
+using Sprint_0.Scripts.Collider.Item;
 
 namespace Sprint_0.Scripts.Items
 {
