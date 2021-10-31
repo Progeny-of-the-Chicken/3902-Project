ad034d0df7249e4042c7e3c76daa7e1be6a6548c
lucasbutlerr <butler.1032@osu.edu>
2021-10-31 11:00:32 -0400

    Added IEffect and EffectFactory.

---
+++ Sprint 0/Scripts/Effect/EffectFactory.cs
@@ -0,0 +1,29 @@
+﻿using Sprint_0.Scripts.Terrain;
+
+namespace Sprint_0.Scripts.Effect
+{
+    public class EffectFactory
+    {
+        private IRoom room;
+
+        private static EffectFactory instance = new EffectFactory();
+
+        public static EffectFactory Instance
+        {
+            get
+            {
+                return instance;
+            }
+        }
+
+        private EffectFactory()
+        {
+        }
+
+        public void LoadRoom(IRoom room)
+        {
+            this.room = room;
+        }
+
+    }
+}
---
+++ Sprint 0/Scripts/Effect/IEffect.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint_0.Scripts.Effect
+{
+    public interface IEffect
+    {
+        void Update(GameTime gameTime);
+
+        void Draw(SpriteBatch spriteBatch);
+
+        bool CheckDelete();
+    }
+}
