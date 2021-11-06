b37aaf96f0066dafa1cfc2fa1ecb493ff1a5dd00
Keith Kriston <59848719+Kriston5@users.noreply.github.com>
2021-11-06 13:57:56 -0400

    Made it so M mutes and unmutes the game's sound

--- Sprint 0/Scripts/Controller/KeyboardController.cs
+++ Sprint 0/Scripts/Controller/KeyboardController.cs
@@ -52,6 +52,8 @@ namespace Sprint_0.Scripts.Controller
 			this.RegisterCommand(linkControllerMappings, Keys.N, new LinkUseSword(game.link, game));
 			this.RegisterCommand(linkControllerMappings, Keys.Z, new LinkUseSword(game.link, game));
 			this.RegisterCommand(linkControllerMappings, Keys.E, new LinkTakeDamage(game.link));
+
+			this.RegisterCommand(controllerMappings, Keys.M, new ToggleMute());
 		}
 
 		//Update checks for keys pressed and calls the respective command
