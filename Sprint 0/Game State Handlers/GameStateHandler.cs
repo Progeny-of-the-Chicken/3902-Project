d17127d1ef19fc547b8b2ac36e0d6ca1a1dd3cfd
Nicholas Brinkel <Nickbrinkel@gmail.com>
2021-11-08 13:57:08 -0500

    Added GameStateHandler interface and the Gameplay specific implimentation of it

---
+++ Sprint 0/Game State Handlers/GameStateHandler.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Sprint_0
+{
+    public interface GameStateHandler
+    {
+        void Update(GameTime gameTime);
+        void Draw(SpriteBatch sb, GameTime gameTime);
+    }
+}
---
+++ Sprint 0/Game State Handlers/GameStateMachine.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Sprint_0.Scripts;
+
+namespace Sprint_0.GameStateHandlers
+{
+    public enum GameState
+    {
+        Menu,
+        Gameplay,
+        Iventory
+    }
+
+    public class GameStateMachine
+    {
+        private GameState _state;
+        private Link link;
+        public GameState state { get => _state; }
+
+        // Game State Handlers
+        GameplayStateHandler gameplay;
+
+        public GameStateMachine(Link link)
+        {
+            // Set initial game state
+            this._state = GameState.Gameplay;
+            this.link = link;
+
+            loadHandlers();
+        }
+
+        public void SetState(GameState newState)
+        {
+            this._state = newState;
+        }
+
+        public void Draw(SpriteBatch sb, GameTime gameTime)
+        {
+            switch (_state)
+            {
+                case GameState.Gameplay:
+                    gameplay.Draw(sb, gameTime);
+                    break;
+                case GameState.Menu:
+                    break;
+                case GameState.Iventory:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            switch (_state)
+            {
+                case GameState.Gameplay:
+                    gameplay.Update(gameTime);
+                    break;
+                case GameState.Menu:
+                    break;
+                case GameState.Iventory:
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        /*--------------- Helper Methods ---------------*/
+
+
+        private void loadHandlers()
+        {
+            this.gameplay = new GameplayStateHandler(this.link);
+        }
+    }
+}
