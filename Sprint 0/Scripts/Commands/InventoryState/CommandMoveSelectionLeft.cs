ea3965050a0d48ab4c599f87e2dce31d5e49b0c1
lucasbutlerr <butler.1032@osu.edu>
2021-11-10 02:42:50 -0500

    Added inventory keyboard controller with associated commands.

---
+++ Sprint 0/Scripts/Commands/InventoryState/CommandMoveSelectionLeft.cs
@@ -0,0 +1,20 @@
+﻿using Sprint_0.Scripts.GameState.InventoryState;
+
+namespace Sprint_0.Scripts.Commands.InventoryState
+{
+    public class CommandMoveSelectionLeft : ICommand
+    {
+        private Game1 game;
+
+        public CommandMoveSelectionLeft(Game1 game)
+        {
+            this.game = game;
+        }
+
+        public void Execute()
+        {
+            // TODO: loop this through game
+            InventoryManager.Instance.MoveSelection(FacingDirection.Left);
+        }
+    }
+}
---
+++ Sprint 0/Scripts/Commands/InventoryState/CommandPauseGame.cs
@@ -0,0 +1,17 @@
+﻿namespace Sprint_0.Scripts.Commands.InventoryState
+{
+    public class CommandPauseGame : ICommand
+    {
+        private Game1 game;
+
+        public CommandPauseGame(Game1 game)
+        {
+            this.game = game;
+        }
+
+        public void Execute()
+        {
+            // TODO: Implement change to pause state here, maybe reuse for game state
+        }
+    }
+}
