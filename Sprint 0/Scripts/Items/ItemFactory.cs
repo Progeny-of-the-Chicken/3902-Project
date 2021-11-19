9245b4aca3b53e8f1b41a67f70cdb66b46570689
lucasbutlerr <butler.1032@osu.edu>
2021-11-18 23:10:10 -0500

    Centered item spawns for initial spawns.

--- Sprint 0/Scripts/Items/ItemFactory.cs
+++ Sprint 0/Scripts/Items/ItemFactory.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using Sprint_0.Scripts.SpriteFactories;
 
 namespace Sprint_0.Scripts.Items
 {
@@ -18,89 +19,89 @@ namespace Sprint_0.Scripts.Items
         {
         }
 
-        public IItem CreateSmallHeartItem(Vector2 location)
+        public IItem CreateSmallHeartItem(Vector2 location, Vector2 spawnerDimensions)
         {
-            return new Item(location, ItemType.SmallHeartItem);
+            return new Item(SpawnHelper.Instance.CenterLocationOnSpawner(location, spawnerDimensions, SpriteRectangles.smallHeartFrames[ObjectConstants.firstFrame].Size.ToVector2() * ObjectConstants.scale), ItemType.SmallHeartItem);
         }
 
-        public IItem CreateHeartContainer(Vector2 location)
+        public IItem CreateHeartContainer(Vector2 location, Vector2 spawnerDimensions)
         {
-            return new Item(location, ItemType.HeartContainer);
+            return new Item(SpawnHelper.Instance.CenterLocationOnSpawner(location, spawnerDimensions, SpriteRectangles.heartContainerFrame.Size.ToVector2() * ObjectConstants.scale), ItemType.HeartContainer);
         }
 
-        public IItem CreateFairy(Vector2 location)
+        public IItem CreateFairy(Vector2 location, Vector2 spawnerDimensions)
         {
-            return new Item(location, ItemType.Fairy);
+            return new Item(SpawnHelper.Instance.CenterLocationOnSpawner(location, spawnerDimensions, SpriteRectangles.fairyFrames[ObjectConstants.firstFrame].Size.ToVector2() * ObjectConstants.scale), ItemType.Fairy);
         }
 
-        public IItem CreateClock(Vector2 location)
+        public IItem CreateClock(Vector2 location, Vector2 spawnerDimensions)
         {
-            return new Item(location, ItemType.Clock);
+            return new Item(SpawnHelper.Instance.CenterLocationOnSpawner(location, spawnerDimensions, SpriteRectangles.clockFrame.Size.ToVector2() * ObjectConstants.scale), ItemType.Clock);
         }
 
-        public IItem CreateBlueRuby(Vector2 location)
+        public IItem CreateBlueRuby(Vector2 location, Vector2 spawnerDimensions)
         {
-            return new Item(location, ItemType.BlueRuby);
+            return new Item(SpawnHelper.Instance.CenterLocationOnSpawner(location, spawnerDimensions, SpriteRectangles.blueRubyFrame.Size.ToVector2() * ObjectConstants.scale), ItemType.BlueRuby);
         }
 
-        public IItem CreateYellowRuby(Vector2 location)
+        public IItem CreateYellowRuby(Vector2 location, Vector2 spawnerDimensions)
         {
-            return new Item(location, ItemType.YellowRuby);
+            return new Item(SpawnHelper.Instance.CenterLocationOnSpawner(location, spawnerDimensions, SpriteRectangles.yellowRubyFrames[ObjectConstants.firstFrame].Size.ToVector2() * ObjectConstants.scale), ItemType.YellowRuby);
         }
 
-        public IItem CreateBasicMapItem(Vector2 location)
+        public IItem CreateBasicMapItem(Vector2 location, Vector2 spawnerDimensions)
         {
-            return new Item(location, ItemType.BasicMapItem);
+            return new Item(SpawnHelper.Instance.CenterLocationOnSpawner(location, spawnerDimensions, SpriteRectangles.basicMapFrame.Size.ToVector2() * ObjectConstants.scale), ItemType.BasicMapItem);
         }
 
-        public IItem CreateBoomerangItem(Vector2 location)
+        public IItem CreateBoomerangItem(Vector2 location, Vector2 spawnerDimensions)
         {
-            return new Item(location, ItemType.BoomerangItem);
+            return new Item(SpawnHelper.Instance.CenterLocationOnSpawner(location, spawnerDimensions, SpriteRectangles.basicBoomerangFrames[ObjectConstants.firstFrame].Size.ToVector2() * ObjectConstants.scale), ItemType.BoomerangItem);
         }
 
-        public IItem CreateBombItem(Vector2 location)
+        public IItem CreateBombItem(Vector2 location, Vector2 spawnerDimensions)
         {
-            return new Item(location, ItemType.BombItem);
+            return new Item(SpawnHelper.Instance.CenterLocationOnSpawner(location, spawnerDimensions, SpriteRectangles.bombItemFrame.Size.ToVector2() * ObjectConstants.scale), ItemType.BombItem);
         }
 
-        public IItem CreateBowItem(Vector2 location)
+        public IItem CreateBowItem(Vector2 location, Vector2 spawnerDimensions)
         {
-            return new Item(location, ItemType.BowItem);
+            return new Item(SpawnHelper.Instance.CenterLocationOnSpawner(location, spawnerDimensions, SpriteRectangles.bowItemFrame.Size.ToVector2() * ObjectConstants.scale), ItemType.BowItem);
         }
 
-        public IItem CreateBasicKey(Vector2 location)
+        public IItem CreateBasicKey(Vector2 location, Vector2 spawnerDimensions)
         {
-            return new Item(location, ItemType.BasicKey);
+            return new Item(SpawnHelper.Instance.CenterLocationOnSpawner(location, spawnerDimensions, SpriteRectangles.basicKeyFrame.Size.ToVector2() * ObjectConstants.scale), ItemType.BasicKey);
         }
 
-        public IItem CreateMagicKey(Vector2 location)
+        public IItem CreateMagicKey(Vector2 location, Vector2 spawnerDimensions)
         {
-            return new Item(location, ItemType.MagicKey);
+            return new Item(SpawnHelper.Instance.CenterLocationOnSpawner(location, spawnerDimensions, SpriteRectangles.magicKeyFrame.Size.ToVector2() * ObjectConstants.scale), ItemType.MagicKey);
         }
 
-        public IItem CreateCompass(Vector2 location)
+        public IItem CreateCompass(Vector2 location, Vector2 spawnerDimensions)
         {
-            return new Item(location, ItemType.Compass);
+            return new Item(SpawnHelper.Instance.CenterLocationOnSpawner(location, spawnerDimensions, SpriteRectangles.compassFrame.Size.ToVector2() * ObjectConstants.scale), ItemType.Compass);
         }
 
-        public IItem CreateTriforcePiece(Vector2 location)
+        public IItem CreateTriforcePiece(Vector2 location, Vector2 spawnerDimensions)
         {
-            return new Item(location, ItemType.TriforcePiece);
+            return new Item(SpawnHelper.Instance.CenterLocationOnSpawner(location, spawnerDimensions, SpriteRectangles.triforcePieceFrames[ObjectConstants.firstFrame].Size.ToVector2() * ObjectConstants.scale), ItemType.TriforcePiece);
         }
 
-        public IItem CreateBasicArrowItem(Vector2 location)
+        public IItem CreateBasicArrowItem(Vector2 location, Vector2 spawnerDimensions)
         {
-            return new Item(location, ItemType.BasicArrowItem);
+            return new Item(SpawnHelper.Instance.CenterLocationOnSpawner(location, spawnerDimensions, SpriteRectangles.basicArrowItemFrame.Size.ToVector2() * ObjectConstants.scale), ItemType.BasicArrowItem);
         }
 
-        public IItem CreateSilverArrowItem(Vector2 location)
+        public IItem CreateSilverArrowItem(Vector2 location, Vector2 spawnerDimensions)
         {
-            return new Item(location, ItemType.SilverArrowItem);
+            return new Item(SpawnHelper.Instance.CenterLocationOnSpawner(location, spawnerDimensions, SpriteRectangles.silverArrowItemFrame.Size.ToVector2() * ObjectConstants.scale), ItemType.SilverArrowItem);
         }
 
-        public IItem CreateBlueRingItem(Vector2 location)
+        public IItem CreateBlueRingItem(Vector2 location, Vector2 spawnerDimensions)
         {
-            return new Item(location, ItemType.BlueRing);
+            return new Item(SpawnHelper.Instance.CenterLocationOnSpawner(location, spawnerDimensions, SpriteRectangles.blueRingItemFrame.Size.ToVector2() * ObjectConstants.scale), ItemType.BlueRing);
         }
     }
 }
