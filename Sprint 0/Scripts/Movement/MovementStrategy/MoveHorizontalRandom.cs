d928e51ee8828c01270db4abee2c4bc758072f7f
lucasbutlerr <butler.1032@osu.edu>
2021-11-21 17:32:14 -0500

    Implemented random vertical movement.

---
+++ Sprint 0/Scripts/Movement/MovementStrategy/MoveHorizontalRandom.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+
+namespace Sprint_0.Scripts.Movement.MovementStrategy
+{
+    public class MoveHorizontalRandom : IMovementStrategy
+    {
+        private float speed;
+        private int moveDistance;
+        private Vector2 startLocation;
+        private Vector2 directionVector;
+
+        public MoveHorizontalRandom(Vector2 directionVector, float speed, int moveDistance)
+        {
+            this.directionVector = directionVector;
+            this.speed = speed;
+            this.moveDistance = moveDistance;
+        }
+
+        public Vector2 Move(GameTime gameTime, Vector2 location)
+        {
+            if (location.X < startLocation.X - moveDistance || location.X >= startLocation.X)
+            {
+                directionVector *= ObjectConstants.vectorFlip;
+            }
+            return location += speed * directionVector * (float)gameTime.ElapsedGameTime.TotalSeconds;
+        }
+    }
+}
