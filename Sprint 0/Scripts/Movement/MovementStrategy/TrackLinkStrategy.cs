e4105767ba335ea781b6132cebe94f255aae1731
lucasbutlerr <butler.1032@osu.edu>
2021-11-21 18:01:55 -0500

    Implemented link tracking movement.

--- Sprint 0/Scripts/Movement/MovementStrategy/TrackLinkStrategy.cs
+++ Sprint 0/Scripts/Movement/MovementStrategy/TrackLinkStrategy.cs
@@ -1,18 +1,26 @@
-﻿using Microsoft.Xna.Framework;
+﻿using System;
+using Microsoft.Xna.Framework;
+using Sprint_0.Scripts.SpriteFactories;
 
 namespace Sprint_0.Scripts.Movement.MovementStrategy
 {
     public class TrackLinkStrategy : IMovementStrategy
     {
-        public TrackLinkStrategy()
+        private float speed;
+        private Vector2 spawnerDimensions;
+
+        public TrackLinkStrategy(float speed, Vector2 spawnerDimensions)
         {
-            // TODO: Add movement constants
+            this.speed = speed;
+            this.spawnerDimensions = spawnerDimensions;
         }
 
         public Vector2 Move(GameTime gameTime, Vector2 location)
         {
-            // TODO: Add movement implementation
-            return location;
+            Vector2 distanceVector = SpawnHelper.Instance.CenterLocationOnSpawner(Link.Instance.Position, new Vector2(ObjectConstants.linkWidthHeight), spawnerDimensions) - location;
+            Vector2 abs = new Vector2(Math.Abs(distanceVector.X), Math.Abs(distanceVector.Y));
+            Vector2 xyScale = new Vector2(distanceVector.X / (abs.X + abs.Y), distanceVector.Y / (abs.X + abs.Y));
+            return location += new Vector2(speed) * xyScale;
         }
     }
 }
