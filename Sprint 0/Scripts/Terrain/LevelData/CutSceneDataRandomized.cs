de9e41351c1a5698da72ced6af0389813db244cb
Nicholas Brinkel <Nickbrinkel@gmail.com>
2021-12-06 23:24:52 -0500

    Added dialogue to randomized dungeon mode

--- Sprint 0/Scripts/Terrain/LevelData/CutSceneDataRandomized.cs
+++ Sprint 0/Scripts/Terrain/LevelData/CutSceneDataRandomized.cs
@@ -1,27 +1,27 @@
-﻿using System.Collections.Generic;
+﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 
 namespace Sprint_0.Scripts.Terrain.LevelData
 {
-    public struct CutSceneData
+    public struct CutSceneDataRandomized
     {
-        private static string[] room25 = {
+        private static string[] room47 = {
             "MANY YEARS AGO PRINCE DARKNESS GANNON STOLE ONE OF THE TRIFORCE WITH POWER",
             "PRINCESS ZELDA HAD ONE OF THE TRIFORCE WITH WISDOM",
             "SHE DIVIDED IT INTO ONE UNIT TO HIDE IT FROM GANNON BEFORE SHE WAS CAPTURED.",
-            "GO FIND THE ONE UNITS LINK TO SAVE HER",
-            "Wait bruh thats a shotgun"
+            "GO FIND THE ONE UNITS LINK TO SAVE HER"
         };
         private static string[] room00 = {
-            "                                                                                                         Bruh thats a                     Bow"
+            "                                                                                                         Bruh thats a                     shotgun"
         };
 
         public static Dictionary<string, string[]> dialogueKeys = new Dictionary<string, string[]>()
         {
-            { "Room25", room25 },
+            { "Room47", room47 },
             { "Room00" , room00 }
         };
-        private static ArrayList suspended = new ArrayList() { "Room25" };
+        private static ArrayList suspended = new ArrayList() { "Room47" };
 
 
 
